86b44431a10f3c532e0ab6358b27c1c9f882b009
jianxiong.xie <jianxiong.xie@starlight.com>
2019-06-17 17:58:16 +0800

    opt router code

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -2,8 +2,8 @@ import { RouterType } from "../enums";
 
 export const routerMapping = (routerType: RouterType) => {
   const mappingList = {
-    [RouterType.Music]: 'pages/music/index',
-    [RouterType.Me]: 'pages/me/index'
+    [RouterType.Music]: '/pages/music/index',
+    [RouterType.Me]: '/pages/me/index'
   }
 
   return mappingList[routerType];
